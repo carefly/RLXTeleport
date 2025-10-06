d286f2313c243c0158e4722196ea2b98029e0d46
carefly <carefly@live.cn>
2025-10-06 11:32:16 +0800

    Adapt for Levilamina 1.6.0

--- xmake.lua
+++ xmake.lua
@@ -6,9 +6,9 @@ add_repositories("liteldev-repo https://github.com/LiteLDev/xmake-repo.git")
 -- add_requires("levilamina develop") to use develop version
 -- please note that you should add bdslibrary yourself if using dev version
 if is_config("target_type", "server") then
-    add_requires("levilamina 1.5.1", {configs = {target_type = "server"}})
+    add_requires("levilamina 1.6.0", {configs = {target_type = "server"}})
 else
-    add_requires("levilamina 1.5.1", {configs = {target_type = "client"}})
+    add_requires("levilamina 1.6.0", {configs = {target_type = "client"}})
 end
 
 add_requires("levibuildscript")
@@ -27,8 +27,8 @@ target("RLXTeleport") -- Change this to your mod name.
     add_rules("@levibuildscript/linkrule")
     add_rules("@levibuildscript/modpacker")
     add_cxflags( "/EHa", "/utf-8", "/W4", "/w44265", "/w44289", "/w44296", "/w45263", "/w44738", "/w45204")
-    add_defines("NOMINMAX", "UNICODE")
-    add_packages("levilamina")
+    add_defines("NOMINMAX", "UNICODE") 
+    add_packages("levilamina") 
     set_exceptions("none") -- To avoid conflicts with /EHa.
     set_kind("shared")
     set_languages("c++20")
