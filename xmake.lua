9c9deaeb054da7ac68dfb4d8190420e6153aeb9f
carefly <carefly@live.cn>
2025-11-08 11:16:26 +0800

    修正版本号

--- xmake.lua
+++ xmake.lua
@@ -1,7 +1,5 @@
 add_rules("mode.debug", "mode.release")
 
-set_version("1.7.0")
-
 add_repositories("liteldev-repo https://github.com/LiteLDev/xmake-repo.git")
 
 -- add_requires("levilamina x.x.x") for a specific version
